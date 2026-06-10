5e7d3a28d9433ec3dbccb42066541fad8cb8fd19
deepak-9962 <210823104027@kingsedu.ac.in>
2026-06-10 19:48:33 +0530

    LeftRotate fix with better user interation

--- src/Arrays/LeftRotate.java
+++ src/Arrays/LeftRotate.java
@@ -5,6 +5,7 @@ public class LeftRotate {
         System.out.println("Enter the size of the array: ");
         int n=sc.nextInt();
         int arr[]=new int[n];
+        System.out.println("enter the array elements: ");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
