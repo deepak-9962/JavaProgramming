b7d6b926ed1a1b31a87e1ce30dfa0c655dd0d091
deepak-9962 <210823104027@kingsedu.ac.in>
2026-06-15 05:40:01 +0530

    Add ReverseWords skeleton and implement MaxFrequency logic

--- src/Strings/MaxFrequency.java
+++ src/Strings/MaxFrequency.java
@@ -1,4 +1,23 @@
-package Strings;
-
+import java.util.*;
 public class MaxFrequency {
+    public static void main(String are[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int max=Integer.MIN_VALUE;
+        char maxchar=str.charAt(0);
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            int count=0;
+            for(int j=0;j<str.length();j++){
+                if(ch==str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count>max){
+                max=count;
+                maxchar=ch;
+            }
+        }
+        System.out.println(maxchar);
+    }
 }
