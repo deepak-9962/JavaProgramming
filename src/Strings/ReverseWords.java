c34f89eb870d3ceca5b19a1dae954e711cbc3bba
deepak-9962 <210823104027@kingsedu.ac.in>
2026-06-15 06:11:51 +0530

    Implement ReverseWords to print words in reverse order from input string

--- src/Strings/ReverseWords.java
+++ src/Strings/ReverseWords.java
@@ -1,4 +1,20 @@
-package Strings;
-
+import java.util.*;
 public class ReverseWords {
+    public static void main(String are[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int end=str.length()-1;
+        for(int i=str.length()-1;i>=0;i--) {
+            if (str.charAt(i) == ' ') {
+                for (int j = i + 1; j <= end; j++) {
+                    System.out.print(str.charAt(j));
+                }
+                System.out.print(" ");
+                end = i - 1;
+            }
+        }
+        for(int j=0;j<=end;j++){
+            System.out.print(str.charAt(j));
+        }
+    }
 }
